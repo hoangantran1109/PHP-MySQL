59b4f098edcee210bfea1e2b8f348cfda93062ac
Hoang An Tran <hoangantran1109@gmail.com>
2021-03-16 11:27:24 +0100

    Add PHPlessons and MySQL Folders

---
+++ phplessons/intro.php
@@ -0,0 +1,29 @@
+<DOCTYPE html>
+<html>
+<body>
+<?php
+echo "Hello World!<hr/>";
+
+//Comment
+
+#Comment
+/*
+Multi-line Comment Block
+*/
+
+// Comments contained inside code
+$x = 10 /* +5 */ + 20;
+echo $x;
+echo "<hr />";
+
+ECHO "Text Sample 1 <hr />";
+echo "Text Sample 2 <hr />";
+ECho "Text Sample 3 <hr />";
+
+$car="volvo";
+echo "My car is a ". $car ."<br>";
+echo "My car is a ". $CAR ."<br>";
+echo "My car is a ". $cAR ."<br>";
+?>
+</body>
+</html>
